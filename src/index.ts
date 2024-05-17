bf24791dab4fb830038674bea665809183612c14
orax <orax@local.loc>
2024-05-18 00:47:19 +0200

    chore: add comments

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
-//Import some assets from Vortex we'll need.
 import path from 'path';
-import { log, selectors, types, util } from 'vortex-api';
+import { types } from 'vortex-api';
 
-// Nexus Mods domain for the game. e.g. nexusmods.com/bloodstainedritualofthenight
 const GAME_ID = 'grounded';
 
-//Steam Application ID, you can get this from https://steamdb.info/apps/
 const STEAM_APPID = '962130';
-// const MS_APPID = ''
+// const MS_APPID = '' //TODO: xbox
 
 const PAK_EXTENSION = '.pak';
 
-const STEAM_BINARIES_PATH = 'Maine\\Binaries\\Win64' // Steam
-// const MS_BINARIES_PATH = 'Maine\\Binaries\\WinGDK'   // Microsoft Windows Store/Xbox Pass
+const DEFAULT_BINARIES_PATH = 'Maine\\Binaries\\Win64'
+const MS_BINARIES_PATH = 'Maine\\Binaries\\WinGDK' // Microsoft Windows Store/Xbox Pass
 
-let BinariesPath = STEAM_BINARIES_PATH
+let BinariesPath = DEFAULT_BINARIES_PATH
 
 function testSupportedContent_ue4ss_lua(files: string[], gameId: string) {
-  // Make sure we're able to support this mod.
-
+  // required: <mod_name>\Scripts\main.lua
   const supported = (gameId === GAME_ID) &&
     (files.find((file: string) => /[^\\]+\\Scripts\\main\.lua$/.test(file)) !== undefined);
 
@@ -29,6 +25,10 @@ function testSupportedContent_ue4ss_lua(files: string[], gameId: string) {
 }
 
 function installContent_ue4ss_lua(files: string[]) {
+  /*
+    UE4SS Lua mods
+  */
+
   // The main.lua file is expected to always be positioned in "<MOD_NAME>\Scripts\main.lua".
   const regex = /[^\\]+\\Scripts\\main\.lua$/
   const modFile = files.find((file: string) => regex.test(file));
@@ -37,8 +37,7 @@ function installContent_ue4ss_lua(files: string[]) {
 
   // Remove directories and anything that isn't in the rootPath.
   const filtered = files.filter((file: string) =>
-  ((file.indexOf(rootPath) !== -1)
-    && (!file.endsWith(path.sep))));
+    ((file.indexOf(rootPath) !== -1) && (!file.endsWith(path.sep))));
 
   const instructions = filtered.map((file: string) => {
     return {
@@ -52,8 +51,7 @@ function installContent_ue4ss_lua(files: string[]) {
 }
 
 function testSupportedContent_ue4ss_cpp(files: string[], gameId: string) {
-  // Make sure we're able to support this mod.
-
+  // required: <mod_name>\dlls\main.dll
   const supported = (gameId === GAME_ID) &&
     (files.find((file: string) => /[^\\]+\\dlls\\main\.dll$/.test(file)) !== undefined);
 
@@ -64,6 +62,10 @@ function testSupportedContent_ue4ss_cpp(files: string[], gameId: string) {
 }
 
 function installContent_ue4ss_cpp(files: string[]) {
+  /*
+    UE4SS C++ mods
+  */
+
   // The main.dll file is expected to always be positioned in "<MOD_NAME>\dlls\main.dll".
   const regex = /[^\\]+\\dlls\\main\.dll$/
   const modFile = files.find((file: string) => regex.test(file));
@@ -87,8 +89,7 @@ function installContent_ue4ss_cpp(files: string[]) {
 }
 
 function testSupportedContent_ue4ss_BPLogicMods(files: string[], gameId: string) {
-  // Make sure we're able to support this mod.
-
+  // required: LogicMods\*.pak
   const supported = (gameId === GAME_ID) &&
     (files.find((file: string) => /LogicMods\\.+\.pak$/.test(file)) !== undefined);
 
@@ -99,6 +100,10 @@ function testSupportedContent_ue4ss_BPLogicMods(files: string[], gameId: string)
 }
 
 function installContent_ue4ss_BPLogicMods(files: string[]) {
+  /*
+    UE4SS Blueprint (.pak) mods
+  */
+
   // The .pak file is expected to always be positioned in "LogicMods\*\*.pak".
   const regex = /LogicMods\\.+\.pak$/
   const modFile = files.find((file: string) => regex.test(file));
@@ -121,7 +126,7 @@ function installContent_ue4ss_BPLogicMods(files: string[]) {
 }
 
 function testSupportedContent_paks(files: string[], gameId: string) {
-  // Make sure we're able to support this mod.
+  // required: *.pak
   const supported = (gameId === GAME_ID) &&
     (files.find((file: string) => path.extname(file).toLowerCase() === PAK_EXTENSION) !== undefined);
 
@@ -132,7 +137,11 @@ function testSupportedContent_paks(files: string[], gameId: string) {
 }
 
 function installContent_paks(files: string[]) {
-  // The .pak file is expected to always be positioned in the Paks directory we're going to disregard anything placed outside the root.
+  /*
+    .pak mods
+  */
+
+  // The .pak file is expected to always be positioned in the Paks directory.
   const modFile = files.find((file: string) => path.extname(file).toLowerCase() === PAK_EXTENSION);
   const idx = modFile.indexOf(path.basename(modFile));
   const rootPath = path.dirname(modFile);
@@ -153,8 +162,7 @@ function installContent_paks(files: string[]) {
   return Promise.resolve({ instructions });
 }
 
-function testSupportedContent_generic(files: string[], gameId: string) {
-  // Make sure we're able to support this mod.
+function testSupportedContent_generic(_files: string[], gameId: string) {
   const supported = (gameId === GAME_ID)
 
   return Promise.resolve({
@@ -164,6 +172,24 @@ function testSupportedContent_generic(files: string[], gameId: string) {
 }
 
 function installContent_generic(files: string[]) {
+  /*
+    Generic mods
+
+    Example:
+
+    Grounded-mod.zip
+    ├── Grounded
+    └── readme.txt
+        └── Maine
+            └── Content
+                └── Movies
+                    └── intro_1080p60.bk2
+    
+    modFile: intro_1080p60.bk2
+    rootPath: Maine
+    intro_1080p60.bk2 will be copied into Maine\Content\Movies\
+  */
+
   const modFile = files.find((file) => !file.endsWith('/'));
   const rootPath = modFile.match(/.*\\?(?:Grounded\\|^)/)[0];
   const idx = rootPath.length;
@@ -183,6 +209,7 @@ function installContent_generic(files: string[]) {
   return Promise.resolve({ instructions });
 }
 
+//TODO: xbox
 // function setup(discovery: types.IDiscoveryResult) {
 //   if (discovery.store === 'xbox') {
 //     BinariesPath = MS_BINARIES_PATH
@@ -196,7 +223,7 @@ function main(context: types.IExtensionContext) {
     mergeMods: true,
     queryArgs: {
       steam: [{ id: STEAM_APPID }],
-      // xbox: [{ id: MS_APPID }]
+      // xbox: [{ id: MS_APPID }] //TODO: xbox
     },
     supportedTools: [],
     queryModPath: () => '.',
@@ -205,7 +232,7 @@ function main(context: types.IExtensionContext) {
     requiredFiles: [
       'Grounded.exe'
     ],
-    // setup: (discovery) => setup(discovery),
+    // setup: (discovery) => setup(discovery), //TODO: xbox
     environment: {
       SteamAPPId: STEAM_APPID,
     },
@@ -215,10 +242,15 @@ function main(context: types.IExtensionContext) {
     requiresCleanup: true,
   });
 
+  // UE4SS Lua mods
   context.registerInstaller('grounded-ue4ss_lua', 25, testSupportedContent_ue4ss_lua, installContent_ue4ss_lua);
+  // UE4SS C++ mods
   context.registerInstaller('grounded-ue4ss_cpp', 30, testSupportedContent_ue4ss_cpp, installContent_ue4ss_cpp);
+  // UE4SS Blueprint mods
   context.registerInstaller('grounded-ue4ss_BPLogicMods', 35, testSupportedContent_ue4ss_BPLogicMods, installContent_ue4ss_BPLogicMods);
+  // .pak mods
   context.registerInstaller('grounded-paks', 40, testSupportedContent_paks, installContent_paks);
+  // Generic mods
   context.registerInstaller('grounded-generic', 90, testSupportedContent_generic, installContent_generic)
 
   return true
